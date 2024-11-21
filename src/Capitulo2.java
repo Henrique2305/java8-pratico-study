1d47e2a21c71274faaddf4d7b905cad4925b4b8e
Henrique Ferreira Silva <henrique_silva_ferreira@hotmail.com>
2024-11-20 21:56:01 -0300

    foreach com classe anonima

--- src/Capitulo2.java
+++ src/Capitulo2.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Capitulo2 {
     public static void main(String[] args) {
@@ -9,8 +10,17 @@ public class Capitulo2 {
 
         List<Usuario> usuarios = Arrays.asList(user1, user2, user3);
 
-        for (Usuario u : usuarios) {
-            System.out.println(u.getNome());
-        }
+//        for (Usuario u : usuarios) {
+//            System.out.println(u.getNome());
+//        }
+
+        /**
+         * Usando uma classe anônima
+         */
+        usuarios.forEach(new Consumer<Usuario>() {
+            public void accept(Usuario u) {
+                System.out.println(u.getNome());
+            }
+        });
     }
 }
